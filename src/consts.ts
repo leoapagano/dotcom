fd49cd8ea43cf8f1a4bce6cc2152b0e665d283a7
Leo Pagano <leo@leoapagano.com>
2025-05-12 21:25:04 -0400

    style: run Prettier (I had no idea this existed wtf)

--- src/consts.ts
+++ src/consts.ts
@@ -1,50 +1,50 @@
-import type { IconMap, SocialLink, Site } from '@/types'
+import type { IconMap, SocialLink, Site } from "@/types"
 
 export const SITE: Site = {
-  title: 'Leo Pagano',
-  description:
-    "I'm currently a rising junior at the University of Connecticut studying Computer Science and Engineering.",
-  href: 'https://leoapagano.com',
-  locale: 'en-US',
-  featuredPostCount: 3,
-  postsPerPage: 10,
+	title: "Leo Pagano",
+	description:
+		"I'm currently a rising junior at the University of Connecticut studying Computer Science and Engineering.",
+	href: "https://leoapagano.com",
+	locale: "en-US",
+	featuredPostCount: 3,
+	postsPerPage: 10,
 }
 
 export const NAV_LINKS: SocialLink[] = [
-  {
-    href: '/blog',
-    label: 'blog',
-  },
-  {
-    href: '/portfolio',
-    label: 'portfolio',
-  },
-  {
-    href: '/contact',
-    label: 'contact me',
-  },
+	{
+		href: "/blog",
+		label: "blog",
+	},
+	{
+		href: "/portfolio",
+		label: "portfolio",
+	},
+	{
+		href: "/contact",
+		label: "contact me",
+	},
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  {
-    href: 'https://github.com/leoapagano',
-    label: 'GitHub',
-  },
-  {
-    href: 'https://linkedin.com/in/leoapagano',
-    label: 'LinkedIn',
-  },
-  {
-    href: '/rss.xml',
-    label: 'RSS',
-  },
+	{
+		href: "https://github.com/leoapagano",
+		label: "GitHub",
+	},
+	{
+		href: "https://linkedin.com/in/leoapagano",
+		label: "LinkedIn",
+	},
+	{
+		href: "/rss.xml",
+		label: "RSS",
+	},
 ]
 
 export const ICON_MAP: IconMap = {
-  Website: 'lucide:globe',
-  GitHub: 'lucide:github',
-  LinkedIn: 'lucide:linkedin',
-  Twitter: 'lucide:twitter',
-  Email: 'lucide:mail',
-  RSS: 'lucide:rss',
+	Website: "lucide:globe",
+	GitHub: "lucide:github",
+	LinkedIn: "lucide:linkedin",
+	Twitter: "lucide:twitter",
+	Email: "lucide:mail",
+	RSS: "lucide:rss",
 }
